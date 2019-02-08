693f2834a95024c3fef70b8c8ec56a8053afe97e
Guillaume Sousa Amaral <guillaume.sousa@nist.gov>
2019-02-08 11:34:31 -0500

    fix: replace fictitious email domain by the one used in the django doc
    Change-Id: I2a1231adaef099f708da2e249740e05ca1448110

--- qdr/settings.py
+++ qdr/settings.py
@@ -184,8 +184,8 @@ connect(DB_NAME, host=MONGODB_URI)
 SERVER_EMAIL = ""
 EMAIL_SUBJECT_PREFIX = ""
 USE_EMAIL = False
-ADMINS = [('admin', 'admin@qdr.org')]
-MANAGERS = [('manager', 'moderator@qdr.org')]
+ADMINS = [('admin', 'admin@example.com')]
+MANAGERS = [('manager', 'moderator@example.com')]
 
 USE_BACKGROUND_TASK = False
 # FIXME: set a redis password in production
